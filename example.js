dbec905f010372ff64dcc66783e24ac4b257b2e7
Julian Gruber <julian@juliangruber.com>
2015-10-05 11:10:04 +0200

    beef up example

--- example.js
+++ example.js
@@ -1,6 +1,17 @@
 var Dump = require('./');
+var bytes = require('bytes');
 
-var b = new Buffer('foo bar \x00 \x00 yolo yo ok thanks computer');
+var size = '10kb';
+
+var len = bytes(size);
+var b = new Buffer(len);
+for (var i = 0; i < len; i++) {
+  b[i] = Math.round(Math.random() * 255);
+}
+
+var start = Date.now();
+
 var d = new Dump(b);
-
 d.appendTo(document.body);
+
+console.log('Took %s ms for %s', Date.now() - start, size);
