106505e696fe047f8416f8984a7b699121b246c3
Julian Gruber <julian@juliangruber.com>
2015-12-01 18:55:37 +0100

    example-simple: faster, more random

--- example/simple.js
+++ example/simple.js
@@ -2,14 +2,15 @@ var Dump = require('..');
 var bytes = require('bytes');
 var Mem = require('memory-chunk-store');
 var shannon = require('binary-shannon-entropy');
+var entro = require('entropy-buffer');
 
-var size = '20mb';
+var size = '1mb';
 
 var len = bytes(size);
 var chunks = Mem(16);
 
 for (var i = 0; i < len / 16; i++) {
-  chunks.put(i, Buffer(16));
+  chunks.put(i, entro(16, Math.random() * 9));
 }
 
 console.log('GO');
